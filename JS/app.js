2c695a756ec9b518e0516c5b60d0de6dd490deba
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-16 14:10:33 +0530

    blurred bg popup

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,18 @@
-function createEl(){
-    
-}
+const containerE1 = document.querySelector('.container');
+
+const btnE1 = document.querySelector('.btn');
+
+const popupContainerE1 = document.querySelector('.popup-container');
+
+const closeIcon = document.querySelector('.close-icon');
+
+btnE1.addEventListener('click',()=>{
+    containerE1.classList.add('active');
+    popupContainerE1.classList.remove('active')
+
+})
+
+closeIcon.addEventListener('click',()=>{
+    containerE1.classList.remove('active');
+    popupContainerE1.classList.add('active')
+})
